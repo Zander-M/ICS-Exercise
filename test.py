8d0ddcfd677306b84da0e816cef5fee882465251
Zander-M <sherlingford@yahoo.com>
2018-03-06 09:30:16 +0800

    adding Q1 Q2

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ Author: Zander.M
 Created at: 14:46, 03, 04, 2018, 
 """
 
-from matplotlib import pyplot as plt
-import math
-
-LIMIT = 100
-x = [i for i in range(LIMIT)]
-y1 = [math.factorial(j) for j in x]
-y2 = [int(k ** k) for k in x]
-plt.ylim(10)
-plt.xlim(10)
-plt.plot(x, y1)
-plt.plot(x, y2)
-plt.legend()
-plt.show()
+# from matplotlib import pyplot as plt
+# import math
+#
+# LIMIT = 100
+# x = [i for i in range(LIMIT)]
+# y1 = [math.factorial(j) for j in x]
+# y2 = [int(k ** k) for k in x]
+# plt.ylim(10)
+# plt.xlim(10)
+# a = plt.plot(x, y1)
+# b = plt.plot(x, y2)
+# plt.legend(a, b)
+# plt.show()
