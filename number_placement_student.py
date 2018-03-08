6c42e00e3ab7b7524de54eb9b4d19a0a79bc52ea
Zander-M <sherlingford@yahoo.com>
2018-03-08 16:18:28 +0800

    adding lab 6 files

--- number_placement_student.py
+++ number_placement_student.py
@@ -7,14 +7,15 @@ class MinMaxQueue:
         self.sorted_q = l
 
     def pop_min(self):
+
         #perform on the min
     
-        return
+        return self.sorted_q.pop(0)
 
     def pop_max(self):
         #perform on the max
 
-        return
+        return self.sorted_q.pop()
 
 def main():
     #create the lists of integers and signs, respectively
@@ -22,12 +23,18 @@ def main():
     li = list(set(li))
     sign_array = ["<" if random.randint(0, 1) else ">" for i in range(len(li) - 1)]
     mmq = MinMaxQueue(li)
-
     result = []
+    for j in sign_array:
+        if j == '<':
+            result.append(mmq.pop_min())
+            result.append(j)
+        else:
+            result.append(mmq.pop_max())
+            result.append(j)
     #decide if you'd take out the min or max of the integer list
     #and append corresponding sign after it
 
-    print(result)
+    print(result[:-1])
 
 main()
 
