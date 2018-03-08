0fd404421c0d6048cff08af4f85faffa5baf3708
Zander-M <sherlingford@yahoo.com>
2018-03-08 16:39:22 +0800

    finished factorial

--- factorial_student.py
+++ factorial_student.py
@@ -1,5 +1,10 @@
 def factorial(num):
-    return
+    if num == 0:
+        return 1
+    elif num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
 
 def main():
     number = int(input("Enter a nonnegative integer: "))
