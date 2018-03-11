c6cc367e961c1e61315f7b393a39dcc2855ab1a4
Zander-M <sherlingford@yahoo.com>
2018-03-11 16:03:17 +0800

    adding maxsum program

--- maxsum_student.py
+++ maxsum_student.py
@@ -14,14 +14,18 @@ class Triangle:
 
     def __init__(self, up_t=None):
         self.up_t = up_t
-        self.size = 1 if up_t == None else up_t.get_size() + 1
-        self.my_row = [random.randint(0, 20) for i in range(self.size)]
+        self.size = 1 if up_t is None else up_t.get_size() + 1
+        self.my_row = [random.randint(0, 20) in range(self.size)]
         self.maxsum = []
 
     def get_size(self):
         return self.size
 
     def get_maxsum(self):
+	    if self.my_row == 1:
+		    return self.my_row
+
+
         #write your code here, and be sure to return what you've done
 
         return 
