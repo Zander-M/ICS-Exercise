9327f0c06e0e8821e752cbadfd061e8ef90ec156
Zander-M <sherlingford@yahoo.com>
2018-03-11 21:09:46 +0800

    finishing pancake sort reversed version

---
+++ Bonus_Exercise.py
@@ -0,0 +1,8 @@
+"""
+Bonus_Exercise
+Author: Zander.M
+Created at: 20:35, 03, 11, 2018, 
+"""
+
+
+"""pancake flipping"""
--- pancake_student.py
+++ pancake_student.py
@@ -15,22 +15,27 @@ class PancakeStack:
 
     # All of the pancakes are sorted after index
     # Returns the index of largest unsorted pancake
-    def find_largest_pancake(self, index):
-        largest_index = index
+    def find_smallest_pancake(self, index):
+        smallest_index = index
+        smallest = self.stack[index]
+        for i in range(index):
+            if self.stack[i] < smallest:
+                smallest = self.stack[i]
+                smallest_index = i
         #write your code here
 
-        return largest_index
+        return smallest_index
 
     # Slide the pan under pancake at desired index and flip to top
     def flip(self, index):
         #write your code here
+        self.stack = self.stack[index::-1] + self.stack[index+1:]
         return
-
 
 def sort_pancakes(pancakes):
     pancakes_size = pancakes.get_size()
     for i in reversed(range(pancakes_size)):
-        flip_index = pancakes.find_largest_pancake(i)
+        flip_index = pancakes.find_smallest_pancake(i)
         pancakes.flip(flip_index)
         if LOGGING: print("Flip Up", pancakes.stack)
         pancakes.flip(i)
